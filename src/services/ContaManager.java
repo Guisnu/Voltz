7642df7ef51df630798da379aa0edb4ecc0c0a46
Eduardo Enrico <143040495+edenrico@users.noreply.github.com>
2024-10-17 11:05:10 -0300

    Update ContaManager.java
    ADD infos sobre cada linha

--- src/services/ContaManager.java
+++ src/services/ContaManager.java
@@ -7,17 +7,17 @@ import java.util.List;
 import java.util.Random;
 
 public class ContaManager {
-    private List<Conta> contas = new ArrayList<>();
-    private Random random = new Random();
+    private List<Conta> contas = new ArrayList<>(); //inicializou uma lista vazia com a variável conta,
+    private Random random = new Random(); //Inicializou a classe RANDOM que é usada para gerar numeros aleatórios em java
 
     public Conta criarConta(String nome, String email, String senha) { //construtor de conta inicializando as strings, o id utilizando RANDOM, e a criação da propria
         Long id = random.nextLong(); // gera um ID aleatório 
-        Conta novaConta = new Conta(id, nome, email, senha);
+        Conta novaConta = new Conta(id, nome, email, senha);  //INICIALIZADOR DO ID, NOME E ETC
         contas.add(novaConta);
         return novaConta; //método de criação pode ser add na main acc utilizando o scanner
     }
 
-    public List<Conta> getContas() {
+    public List<Conta> getContas() {     //TODO ESSE CÓDIGO (ContaManager) VAI SER INICIALIZADO NA MAIN PARA PROCESSO DE CRIAÇÃO DA CONTA!
         return contas; // teste
     }
 }
