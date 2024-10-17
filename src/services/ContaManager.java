4561f3dc3d762417942817950e59959f29d4e6c1
Eduardo Enrico <143040495+edenrico@users.noreply.github.com>
2024-10-17 10:43:14 -0300

    Update ContaManager.java
    criei um service para isolarmos o acesso da criação da conta com o id, implementação da lógica random, criação e listagem das contas..

--- src/services/ContaManager.java
+++ src/services/ContaManager.java
@@ -1,1 +1,23 @@
+package services;
 
+import model.Conta;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class ContaManager {
+    private List<Conta> contas = new ArrayList<>();
+    private Random random = new Random();
+
+    public Conta criarConta(String nome, String email, String senha) { //construtor de conta inicializando as strings, o id utilizando RANDOM, e a criação da propria
+        Long id = random.nextLong(); // gera um ID aleatório 
+        Conta novaConta = new Conta(id, nome, email, senha);
+        contas.add(novaConta);
+        return novaConta; //método de criação pode ser add na main acc utilizando o scanner
+    }
+
+    public List<Conta> getContas() {
+        return contas; // teste
+    }
+}
