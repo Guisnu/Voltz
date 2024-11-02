917936cc20b28f30d69a7a519f4fc4dda16ca618
Guilherme Nunes <123277481+Guisnu@users.noreply.github.com>
2024-11-02 12:43:18 -0300

    fix: herança mercadoService

--- src/services/BaseService.java
+++ src/services/BaseService.java
@@ -1,7 +1,15 @@
 package services;
 
+import model.Criptoativo;
+
+import java.util.List;
+
 public abstract class BaseService {
 
     public double executar(Object param){return 0;};
+
+    public List executar() {
+        return null;
+    }
 
 }
