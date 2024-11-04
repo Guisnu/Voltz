82ee43f871e199309a38a104cfbe5197b39553e1
Willian <williangoncalves74@gmail.com>
2024-11-04 08:37:27 -0300

    adicionado classe Cadastro como intermediaria para Empresas e Investidor

--- src/services/ImpostoSobreVenda.java
+++ src/services/ImpostoSobreVenda.java
@@ -1,17 +1,15 @@
 package services;
 
-public class ImpostoSobreVenda extends BaseService{
+public class ImpostoSobreVenda extends BaseService {
 
 //    public static double calcularImpostoSobreVenda(double valorVenda) {
 //        double taxaVenda = 0.02; // Taxa padrão de 2%
 //        return valorVenda * taxaVenda;
 //    }
-
-
     @Override
     public double executar(Object param) {
         double taxaVenda = 0.02; // Taxa padrão de 2%
-        double resultadoTaxaVenda = (double)param * taxaVenda;;
+        double resultadoTaxaVenda = (double) param * taxaVenda;
         return resultadoTaxaVenda;
     }
 }
