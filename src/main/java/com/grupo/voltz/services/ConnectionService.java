4d4c1f2db97aff703d9ba4c896b7b5823051812a
Guilherme Nunes <123277481+Guisnu@users.noreply.github.com>
2025-03-07 17:14:59 -0300

    ajuste finos main

--- src/main/java/com/grupo/voltz/services/ConnectionService.java
+++ src/main/java/com/grupo/voltz/services/ConnectionService.java
@@ -8,9 +8,9 @@ public class ConnectionService {
 
     private static final String URL = "jdbc:oracle:thin:@oracle.fiap.com.br:1521:orcl";
 
-    private static final String USUARIO = "rm554327"; //Todo: user sqlserver oracle com "rm ou pf" na frente;
+    private static final String USUARIO = ""; //Todo: user sqlserver oracle com "rm ou pf" na frente;
 
-    private static final String SENHA = "020604"; //Todo: senha sqlserver oracle;
+    private static final String SENHA = ""; //Todo: senha sqlserver oracle;
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL,USUARIO,SENHA);
